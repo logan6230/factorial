6072be499f8c2f6a59152da176c40ec4d728aa8e
logan6230 <pantoja2187@gmail.com>
2023-03-09 07:32:37 -0500

    Version 2.0

--- factorial.js
+++ factorial.js
@@ -1,3 +1,12 @@
 function factorial() {
-    let n = document.getElementById('n');
+    let n = parseInt(document.getElementById('n').value);
+    let factorial = n;
+    let i = 2;
+
+    while (i <= n) {
+        factorial = factorial * i;
+        i += 1;
+    }
+    console.log(factorial);
+    return factorial;
 }
